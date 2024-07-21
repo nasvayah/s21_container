d82afd70e7c2a7a6a8a5dc4bff6c5708c0d5eca0
Armond Vickie <armondvi@pr-c2.kzn.21-school.ru>
2024-07-21 20:57:23 +0300

    ff

---
+++ list.h
@@ -0,0 +1,22 @@
+#ifndef LIST_H
+#define LIST_H
+
+namespace s21 {
+
+template <typename T>
+class list {
+    public:
+    using value_type = T;
+    using reference = T&;
+    using const_reference = const T&;
+    
+    private:
+
+
+}
+
+
+}
+
+
+#endif //LIST_H
